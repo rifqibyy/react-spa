6fffd72d23bf6a3fd4b8e7676ab227e5aa7722a7
Rifqi Abiyyu <rifqibyy@gmail.com>
2019-03-18 12:01:10 +0700

    Updates

---
+++ precache-manifest.8c11ac65c02d17a59adfa337df327f39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e306d59b7e4046b4373",
+    "url": "/react-spa/static/js/runtime~main.97fb65bb.js"
+  },
+  {
+    "revision": "504975e99af42f590e72",
+    "url": "/react-spa/static/js/main.b689eecc.chunk.js"
+  },
+  {
+    "revision": "876c61ac4fd85b7921fd",
+    "url": "/react-spa/static/js/2.ba768b5f.chunk.js"
+  },
+  {
+    "revision": "504975e99af42f590e72",
+    "url": "/react-spa/static/css/main.22adb2c5.chunk.css"
+  },
+  {
+    "revision": "d60d776e32aa5c666c4833dc15eeabfb",
+    "url": "/react-spa/index.html"
+  }
+];
